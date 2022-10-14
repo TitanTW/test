ce4d16da6dee5f2dbec1023aa3d971c14b00fc26
TitanTW <115152497+TitanTW@users.noreply.github.com>
2022-10-14 10:01:34 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,50 +1,61 @@
 import random
-print(random,randint(1,3))
 
 location = ["witchPlace", "vampireCastle", "weaponStorage"]
 
 weaponList = ["sword", "rifle"]
 weaponDamage = [300, 500]
 myhealth = 3500
+
 monsterList = ["vampire", "witch"]
 monsterHealth = [1500, 2500]
 monsterAttack = [500,700]
+
+
 def callMonster(in_location):
-    monsterInPlace = {}
+
+    global monsterInPlace_name
+    global monsterInPlace_health
+    global monsterInPlace_attack
+    
+    monsterInPlace_name = ""
+    monsterInPlace_health = 0
+    monsterInPlace_attack = 0
+
+
     if in_location == "witchPlace":
-        monsterInPlace += monsterList[2]
-        monsterInPlace += monsterHealth[2]
-        monsterInPlace += monsterAttack[2]
+        monsterInPlace_name += monsterList[1]
+        monsterInPlace_health += monsterHealth[1]
+        monsterInPlace_attack += monsterAttack[1]
+        
     elif in_location == "vampireCastle":
-        monsterInPlace += monsterList[1]
-        monsterInPlace += monsterHealth[1]
-        monsterInPlace += monsterAttack[1]
+        monsterInPlace_name += monsterList[0]
+        monsterInPlace_health += monsterHealth[0]
+        monsterInPlace_attack += monsterAttack[0]
 
 
 def callWeapon():
     weapon = ""
     for i in weaponList:
-        weapon += i + " "
-    selectWeapon = input("Choose the weapon: " + weapon + "- ")
-    if selectWeapon == "sword":
+        weapon += str(weaponList.index(i) + 1) + ". " + i + "\n"
+    selectWeapon = input("Choose the weapon: " + "\n" + weapon + "- ")
+    if selectWeapon == "1":
+        selectWeapon += weaponList[0]
+        selectWeapon += weaponDamage[0]
+    if selectWeapon == "rifle":
         selectWeapon += weaponList[1]
         selectWeapon += weaponDamage[1]
-    if selectWeapon == "rifle":
-        selectWeapon += weaponList[2]
-        selectWeapon += weaponDamage[2]
     
     return(selectWeapon)
 
 def fightScene():
     while monsterHealth > 0:
-        monsterHealth - weaponDamage
-        myhealth - monsterAttack
-        
- 
 
-
-
-
-
-
-
+        temp = random.randint(1,2)
+        if temp == 1:
+            monsterHealth -= weaponDamage #monster's health subtracted by weapon damage
+            
+        elif temp == 2:
+            myhealth -= monsterAttack #health decreasing from monster attack
+    
+callMonster(location[0])
+print(monsterInPlace_name, str(monsterInPlace_health), str(monsterInPlace_attack))
